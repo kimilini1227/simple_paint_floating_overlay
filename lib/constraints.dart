98ec2374add8cabca7c9683bc9eedc9d6f316f20
kimilini1227 <amanogawa1227@gmail.com>
2024-09-11 02:37:14 +0900

    Handle for issue #25.
    Added overlay window size limit.
    Improve dragging lower edge process.

--- lib/constraints.dart
+++ lib/constraints.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
 
 abstract final class Constraints{
+  // Initial overlay's constraint ratio to full screen size.
   static const double overlayWidthRatio = 0.8;
   static const double overlayHeightRatio = 0.8;
   static const double overlayXPositionRatio = 0.1;
   static const double overlayYPositionRatio = 0.1;
 
+  // Overlay theme color
   static const Color overlayWindowMainColor = Colors.yellow;
   static const Color overlayWindowSubColor = Colors.black;
 
+  // Overlay window
   static const double overlayWindowBorderWidth = 1.0;
+  static const double overlayWindowMinimumWidth = 250.0;
+  static const double overlayWindowMinimumHeight = 50.0;
+
+  // Overlay top bar
   static const double overlayTopBarHeight = 48.0;
 
+  // Overlay minimize button
   static const double overlayMinimizeButtonBorderWidth = 1.0;
 
+  // Overlay lower right edge
   static const double overlayLowerRightEdgeWidth = 12.0;
   static const double overlayLowerRightEdgeHeight = 12.0;
 }
